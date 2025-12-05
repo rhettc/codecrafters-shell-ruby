576c2c926a8cd4a822b53286b5213b55c7b726d8
rhettc <rhett.c@gmail.com>
2025-12-05 15:56:31 -0600

    Implement rudimentary REPL structure

--- app/main.rb
+++ app/main.rb
@@ -1,13 +1,34 @@
 require_relative 'command_resolver'
 require_relative 'errors/unrecognized_command_error'
 
-$stdout.write("$ ")
-input = gets.chomp
-exit 0 if input.nil?
-command, args = input.split
-
-begin
-  resolved_command = CommandResolver.new(command).resolve
-rescue UnrecognizedCommandError
-  $stderr.write "#{command}: not found"
+def read
+  $stdout.print("$ ")
+  #$stdout.flush
+  gets.chomp
 end
+
+def evaluate(input)
+  return if input.nil?
+  command, args = input.split
+
+  begin
+    resolved_command = CommandResolver.new(command).resolve
+    output = resolved_command.execute(args)
+  rescue UnrecognizedCommandError
+    $stderr.puts "#{command}: not found"
+    return nil
+  end
+
+end
+
+def repl
+  begin
+    input = read
+    next if input.empty?
+
+    output = evaluate(input)
+    print "#{output}\n" if output
+  end while true
+end
+
+repl
