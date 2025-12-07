48ba4da247330ffc7a2d0083788312ee3bae398b
rhettc <rhett.c@gmail.com>
2025-12-06 19:25:21 -0600

    Commands have name attributes

--- app/commands/builtin_command.rb
+++ app/commands/builtin_command.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class BuiltinCommand < BasicCommand
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 end
--- app/commands/exit_command.rb
+++ app/commands/exit_command.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class ExitCommand < BuiltinCommand
-  def initialize()
-  end
-
   def execute(args)
     args ||= [0]
     exit_code = args.first&.to_i || 0
